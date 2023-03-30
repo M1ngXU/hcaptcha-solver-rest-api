17ef2f7020f967b2666615ce4d4e9d9a86f5c7fc
M1ngXU <91617361+M1ngXU@users.noreply.github.com>
2023-03-30 14:56:31 +0000

    objects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate tokio;
+#[macro_use]
+extern crate tracing;
+
+use std::collections::HashMap;
+
+use anyhow::Result;
+use cached::proc_macro::once;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct LabelAlias {
+    en: Vec<String>,
+    zh: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Objects {
+    label_alias: HashMap<String, LabelAlias>,
+}
+
+#[once(time = 86400, result = true)]
+#[instrument]
+async fn get_objects() -> Result<Objects> {
+    Ok(serde_yaml::from_slice(
+        &reqwest::get("https://github.com/QIN2DIM/hcaptcha-challenger/raw/main/src/objects.yaml")
+            .await?
+            .bytes()
+            .await?
+            .to_vec(),
+    )?)
+}
+
+#[main]
+async fn main() {
+    println!("{:?}", get_objects().await);
 }
